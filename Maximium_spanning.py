71542438e4d3388db95cd89a1c71e60b7b015625
Khubaib Lodhi <lodhikhubaib12@gmail.com>
2023-10-27 17:57:59 +0500

    some changes

--- Maximium_spanning.py
+++ Maximium_spanning.py
@@ -52,7 +52,7 @@ class Graph:
 class GUI:
     def __init__(self):
         self.window = tk.Tk()
-        icon_image = tk.PhotoImage(file=r"C:\Users\HP\OneDrive\Desktop\Gt Project\connected\logo.png")
+        icon_image = tk.PhotoImage(file=r"C:\Users\HP\OneDrive\Desktop\Graph_Theory_Project\Graph_Theory_Project\src\logo.png")
         self.window.iconphoto(True, icon_image)
         self.window.title("Maximum Spanning Tree")
         self.canvas = tk.Canvas(self.window, width=600, height=400)
